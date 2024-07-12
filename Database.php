b8e5191091b33a469a206ee977c2b3acf7da0092
Giovanni <giovanni.sokenou2105@gmail.com>
2024-07-12 19:46:38 +0200

    initial

---
+++ Database.php
@@ -0,0 +1,74 @@
+<?php
+// EXPORTATION DES VARIABLES D'ENVIRONNEMENTS
+
+require_once("./config/env.php");
+
+abstract class DataBase {
+     
+//CONNECTION A LA BASE DE DONNEE.
+protected function connectDb()
+{
+
+     try {
+          $bdd = new PDO("mysql:host=" . $_ENV['host'] . ";dbname=" . $_ENV['dbname'], $_ENV['username'], $_ENV['password']);
+          $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "connexion ok";
+          return $bdd;
+     } catch (PDOException $e) {
+          echo $e->getMessage();
+     }
+}
+//INSERTION DANS LA BASE DE DONNEE
+protected function insertDb($data, $sql)
+{
+     try {
+          $objBdd = $this->connectDb();
+          $sql = "INSERT INTO  users (username,password) VALUE (?,?)";
+          $req = $objBdd->prepare($sql);
+          $req->execute($data);
+          echo "Utilisateur ajouter avec succès...";
+     } catch (PDOException $th) {
+          echo $th->getMessage();
+     }
+}
+
+// $sql = "INSERT INTO users (username ,password) VALUE (?,?)";
+// $username ="Jaures";
+// $password ="Azerty";
+// $data = [$username,$password];
+// insertDb($data,$sql);
+
+// FONCTION POUR RECUPERER UN USER
+protected function getOneData($sql, $data)
+{
+     try {
+          $objBdd = $this->connectDb();
+          $req = $objBdd->prepare($sql);
+          $req->execute($data);
+          $reponse = $req->fetch(PDO::FETCH_ASSOC);
+          return $reponse;
+          echo "Requete reussie";
+     } catch (PDOException $ta) {
+          echo $ta->getMessage();
+     }
+}
+
+
+
+//RECUPERER TOUS LES USERS
+
+protected function getManyData($sql, $data=null)
+{
+     try {
+          $objBdd = $this->connectDb();
+          $req = $objBdd->prepare($sql);
+          $req->execute($data);
+          $reponse = $req->fetchAll(PDO::FETCH_ASSOC);
+          return $reponse;
+          echo "Requete reussie";
+     } catch (PDOException $ta) {
+          echo $ta->getMessage();
+     }
+}
+
+}
---
+++ inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Inscription</title>
+</head>
+<body>
+<form action="">
+          <fieldset>
+               <legend>Inscription</legend>
+               <input type="text" value="Username">
+               <input type="text" value="Password">
+               <button>Connexion</button>
+          </fieldset>
+     </form>
+     <a href="connexion.php"></a>
+</body>
+</html>
